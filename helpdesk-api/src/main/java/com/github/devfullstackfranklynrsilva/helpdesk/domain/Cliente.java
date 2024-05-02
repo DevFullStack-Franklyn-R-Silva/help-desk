5c23d1cec12e17c81c73b846e933542bf6cab8ff
Franklyn Roberto da Silva <franklyn1998@live.com>
2024-05-02 17:54:04 -0300

    feat - CRUD para Cliente

--- helpdesk-api/src/main/java/com/github/devfullstackfranklynrsilva/helpdesk/domain/Cliente.java
+++ helpdesk-api/src/main/java/com/github/devfullstackfranklynrsilva/helpdesk/domain/Cliente.java
@@ -2,11 +2,13 @@ package com.github.devfullstackfranklynrsilva.helpdesk.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.github.devfullstackfranklynrsilva.helpdesk.domain.dtos.ClienteDTO;
 import com.github.devfullstackfranklynrsilva.helpdesk.domain.enums.Perfil;
 
 import lombok.Getter;
@@ -15,24 +17,32 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
-public class Cliente extends Pessoa{
+public class Cliente extends Pessoa {
 	private static final long serialVersionUID = 1L;
-	
+
 	@JsonIgnore
 	@OneToMany(mappedBy = "cliente")
 	private List<Chamado> chamados = new ArrayList<>();
 
-	
 	public Cliente() {
 		super();
 		addPerfis(Perfil.CLIENTE);
 	}
 
-
 	public Cliente(Integer id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
 		addPerfis(Perfil.CLIENTE);
 	}
-	
-	
+
+	public Cliente(ClienteDTO obj) {
+		super();
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.cpf = obj.getCpf();
+		this.email = obj.getEmail();
+		this.senha = obj.getSenha();
+		this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+		this.dataCriacao = obj.getDataCriacao();
+	}
+
 }
---
+++ helpdesk-api/src/main/java/com/github/devfullstackfranklynrsilva/helpdesk/services/ClienteService.java
@@ -0,0 +1,72 @@
+package com.github.devfullstackfranklynrsilva.helpdesk.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.github.devfullstackfranklynrsilva.helpdesk.domain.Cliente;
+import com.github.devfullstackfranklynrsilva.helpdesk.domain.Pessoa;
+import com.github.devfullstackfranklynrsilva.helpdesk.domain.dtos.ClienteDTO;
+import com.github.devfullstackfranklynrsilva.helpdesk.repositories.ClienteRepository;
+import com.github.devfullstackfranklynrsilva.helpdesk.repositories.PessoaRepository;
+import com.github.devfullstackfranklynrsilva.helpdesk.services.exceptions.DataIntegrityViolationException;
+import com.github.devfullstackfranklynrsilva.helpdesk.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repository;
+
+	@Autowired
+	private PessoaRepository pessoaRepository;
+
+	public Cliente findById(Integer id) {
+		Optional<Cliente> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Obejto não encontrado! Id: " + id));
+	}
+
+	public List<Cliente> findALL() {
+		return repository.findAll();
+	}
+
+	public Cliente create(ClienteDTO objDTO) {
+		objDTO.setId(null);
+		validaPorCpfEmail(objDTO);
+		Cliente newObj = new Cliente(objDTO);
+		return repository.save(newObj);
+	}
+
+	public Cliente update(Integer id, @Valid ClienteDTO objDTO) {
+		objDTO.setId(id);
+		Cliente oldObj = findById(id);
+		validaPorCpfEmail(objDTO);
+		oldObj = new Cliente(objDTO);
+		return repository.save(oldObj);
+	}
+
+	public void delete(Integer id) {
+		Cliente obj = findById(id);
+		if (obj.getChamados().size() > 0) {
+			throw new DataIntegrityViolationException("Cliente possui ordens de serviço e nãaao pode ser deletado!");
+		}
+		repository.deleteById(id);
+	}
+
+	private void validaPorCpfEmail(ClienteDTO objDTO) {
+		Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
+		if (obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+			throw new DataIntegrityViolationException("CPF já cadastrado no sistema!");
+		}
+
+		obj = pessoaRepository.findByEmail(objDTO.getEmail());
+		if (obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+			throw new DataIntegrityViolationException("E-mail já cadastrado no sistema!");
+		}
+	}
+
+}
