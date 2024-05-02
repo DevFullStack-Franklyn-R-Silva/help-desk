7f76bfb4f03c0e63a0c041308900fe3335737d86
Franklyn Roberto da Silva <franklyn1998@live.com>
2024-05-02 17:13:01 -0300

    feat - Exceção para CPF já cadastrado

--- helpdesk-api/src/main/java/com/github/devfullstackfranklynrsilva/helpdesk/repositories/PessoaRepository.java
+++ helpdesk-api/src/main/java/com/github/devfullstackfranklynrsilva/helpdesk/repositories/PessoaRepository.java
@@ -1,9 +1,14 @@
 package com.github.devfullstackfranklynrsilva.helpdesk.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.github.devfullstackfranklynrsilva.helpdesk.domain.Pessoa;
 
-public interface PessoaRepository extends JpaRepository<Pessoa, Integer>{
+public interface PessoaRepository extends JpaRepository<Pessoa, Integer> {
 
+	Optional<Pessoa> findByCpf(String cpf);
+
+	Optional<Pessoa> findByEmail(String email);
 }
--- helpdesk-api/src/main/java/com/github/devfullstackfranklynrsilva/helpdesk/services/TecnicoService.java
+++ helpdesk-api/src/main/java/com/github/devfullstackfranklynrsilva/helpdesk/services/TecnicoService.java
@@ -6,9 +6,12 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.github.devfullstackfranklynrsilva.helpdesk.domain.Pessoa;
 import com.github.devfullstackfranklynrsilva.helpdesk.domain.Tecnico;
 import com.github.devfullstackfranklynrsilva.helpdesk.domain.dtos.TecnicoDTO;
+import com.github.devfullstackfranklynrsilva.helpdesk.repositories.PessoaRepository;
 import com.github.devfullstackfranklynrsilva.helpdesk.repositories.TecnicoRepository;
+import com.github.devfullstackfranklynrsilva.helpdesk.services.exceptions.DataIntegrityViolationException;
 import com.github.devfullstackfranklynrsilva.helpdesk.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -16,6 +19,9 @@ public class TecnicoService {
 
 	@Autowired
 	private TecnicoRepository repository;
+
+	@Autowired
+	private PessoaRepository pessoaRepository;
 
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = repository.findById(id);
@@ -28,7 +34,20 @@ public class TecnicoService {
 
 	public Tecnico create(TecnicoDTO objDTO) {
 		objDTO.setId(null);
+		validaPorCpfEmail(objDTO);
 		Tecnico newObj = new Tecnico(objDTO);
 		return repository.save(newObj);
+	}
+
+	private void validaPorCpfEmail(TecnicoDTO objDTO) {
+		Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
+		if (obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+			throw new DataIntegrityViolationException("CPF já cadastrado no sistema!");
+		}
+		
+		obj = pessoaRepository.findByEmail(objDTO.getEmail());
+		if (obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+			throw new DataIntegrityViolationException("E-mail já cadastrado no sistema!");
+		}
 	}
 }
