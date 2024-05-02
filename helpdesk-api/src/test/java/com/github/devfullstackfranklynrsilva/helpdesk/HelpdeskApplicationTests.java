59f1c85f1df50e131702d7a0c1f73b8a360aade5
Franklyn Roberto da Silva <franklyn1998@live.com>
2024-05-02 12:36:25 -0300

    First commit

---
+++ helpdesk-api/src/test/java/com/github/devfullstackfranklynrsilva/helpdesk/HelpdeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.devfullstackfranklynrsilva.helpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpdeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
