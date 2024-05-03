253050d9f6ed5668cc9317f998326044513abc05
Franklyn Roberto da Silva <franklyn1998@live.com>
2024-05-03 11:50:47 -0300

    fix - reverter o commit

--- helpdesk-api/src/main/java/com/github/devfullstackfranklynrsilva/helpdesk/resources/ChamadoResource.java
+++ helpdesk-api/src/main/java/com/github/devfullstackfranklynrsilva/helpdesk/resources/ChamadoResource.java
@@ -11,7 +11,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -46,12 +45,6 @@ public class ChamadoResource {
 		Chamado newObj = service.create(objDTO);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newObj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
-	}
-
-	@PutMapping(value = "/{id}")
-	public ResponseEntity<ChamadoDTO> update(@PathVariable Integer id, @Valid @RequestBody ChamadoDTO objDTO) {
-		Chamado newObj = service.update(id, objDTO);
-		return ResponseEntity.ok().body(new ChamadoDTO(newObj));
 	}
 
 }
